68ecd16b1d51d7f7065ef289bf598f6c6cd46fbf
KavishanSukumar <kavishansukumar@gmail.com>
2021-09-23 12:20:13 +0530

    add registrationform-customer-individual.html, renamed index.html to index.jsp, deleted src/main/webapp/resources/js/registrationform-customer-company.js

--- src/main/webapp/resources/js/registrationform-customer-company.js
+++
@@ -1,7 +0,0 @@
-                 const actualBtn = document.getElementById('actual-btn');
-
-                  const fileChosen1 = document.getElementById('file-chosen');
-
-                  actualBtn.addEventListener('change', function(){
-                 fileChosen.textContent = this.files[0].name
-                })
