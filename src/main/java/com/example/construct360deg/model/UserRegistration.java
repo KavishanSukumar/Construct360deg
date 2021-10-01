fa21b8d22b31e522e3c5b64b14bb28e6a5ae480d
KavishanSukumar <kavishansukumar@gmail.com>
2021-10-01 14:26:57 +0530

    Registration of the four users

---
+++ src/main/java/com/example/construct360deg/model/UserRegistration.java
@@ -0,0 +1,49 @@
+package com.example.construct360deg.model;
+
+public class UserRegistration {
+    private String email;
+    private String username;
+    private String password;
+    private String contactno;
+    private String professionalrole;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getContactno() {
+        return contactno;
+    }
+
+    public void setContactno(String contactno) {
+        this.contactno = contactno;
+    }
+
+    public String getProfessionalrole() {
+        return professionalrole;
+    }
+
+    public void setProfessionalrole(String professionalrole) {
+        this.professionalrole = professionalrole;
+    }
+}
