cb259f0cf9537bf426162c0842f2d6a35f4067fc
KavishanSukumar <kavishansukumar@gmail.com>
2021-10-12 19:48:26 +0530

    logout servlet

---
+++ src/main/java/com/example/construct360deg/controllers/LogoutSevlet.java
@@ -0,0 +1,19 @@
+package com.example.construct360deg.controllers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+@WebServlet("/Logout")
+public class LogoutSevlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session=req.getSession();
+        session.removeAttribute("uname");
+        resp.sendRedirect(req.getContextPath()+"/login");
+    }
+}
