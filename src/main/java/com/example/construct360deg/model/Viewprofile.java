ab596f44aafc4120705b2ed4bd367dbbe41cc7f6
Chathuri Priyangika <wscpriyangika@gmail.com>
2021-12-17 13:15:36 +0530

    Add summary box

--- src/main/java/com/example/construct360deg/model/Viewprofile.java
+++ src/main/java/com/example/construct360deg/model/Viewprofile.java
@@ -1,2 +1,34 @@
-package com.example.construct360deg.model;public class Viewprofile {
+package com.example.construct360deg.model;
+
+import java.util.ArrayList;
+
+public class Viewprofile {
+    private  String task;
+    private  int userid;
+    private  String summaryText;
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getSummaryText() {
+        return summaryText;
+    }
+
+    public void setSummaryText(String summaryText) {
+        this.summaryText = summaryText;
+    }
+
 }
