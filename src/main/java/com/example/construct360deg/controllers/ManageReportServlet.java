8feb788bd60bb135d8cd018fbf83beb03cc3aa7b
KavishanSukumar <kavishansukumar@gmail.com>
2021-12-18 23:46:49 +0530

    View inquires

--- src/main/java/com/example/construct360deg/controllers/ManageReportServlet.java
+++ src/main/java/com/example/construct360deg/controllers/ManageReportServlet.java
@@ -1,5 +1,8 @@
 package com.example.construct360deg.controllers;
 
+import com.example.construct360deg.dao.ComplainDAO;
+import com.example.construct360deg.model.Complain;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,11 +10,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-@WebServlet("/report")
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@WebServlet("/inquires")
 public class ManageReportServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ArrayList<Complain> complains=new ArrayList<>();
+        ComplainDAO complainDAO=new ComplainDAO();
+
+        try {
+            complains=complainDAO.retriveComplaints();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        req.setAttribute("complains",complains);
         RequestDispatcher requestDispatcher=req.getRequestDispatcher("/html/admin/html/manageinquires.jsp");
         requestDispatcher.forward(req,resp);
     }
+
 }
