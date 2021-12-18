4dd8ab56098d359f741fa542314bca0c0101687a
KavishanSukumar <kavishansukumar@gmail.com>
2021-12-19 00:11:45 +0530

    Renaming managecomplaintservlet

--- src/main/java/com/example/construct360deg/controllers/ManageComplaintServlet.java
+++ src/main/java/com/example/construct360deg/controllers/ManageComplaintServlet.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 @WebServlet("/inquires")
-public class ManageReportServlet extends HttpServlet {
+public class ManageComplaintServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ArrayList<Complain> complains=new ArrayList<>();
