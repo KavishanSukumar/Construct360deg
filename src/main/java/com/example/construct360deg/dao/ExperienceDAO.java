3b1a3fbd572f01c04f00f9b9f7a629085e62f3ee
Chathuri Priyangika <wscpriyangika@gmail.com>
2021-12-21 01:20:21 +0530

    Add experience in view profile-professionals

--- src/main/java/com/example/construct360deg/dao/ExperienceDAO.java
+++ src/main/java/com/example/construct360deg/dao/ExperienceDAO.java
@@ -1,2 +1,62 @@
-package com.example.construct360deg.dao;public class ExperienceDAO {
+package com.example.construct360deg.dao;
+
+import com.example.construct360deg.database.Database;
+import com.example.construct360deg.model.Experience;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ExperienceDAO {
+    public boolean addexperience(Experience experience) throws SQLException {
+        Connection connection = Database.getConnection();
+        String sql ="INSERT INTO `experience`(`title`, `companyname`, `location`, `years`) VALUES (?,?,?,?)";
+        PreparedStatement preparedStatement = null;
+        int row=0;
+
+        preparedStatement= connection.prepareStatement(sql);
+        preparedStatement.setString(1,experience.getTitle());
+        preparedStatement.setString(2,experience.getCompanyname());
+        preparedStatement.setString(3,experience.getLocation());
+        preparedStatement.setInt(4,experience.getYears());
+        row=preparedStatement.executeUpdate();
+
+        if (row>0){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public ArrayList<Experience> retriveExperience() throws SQLException {
+        ArrayList<Experience> experiences = new ArrayList<>();
+        Connection connection = Database.getConnection();
+        PreparedStatement preparedStatement = null;
+        String sql = "SELECT * FROM `experience`";
+        ResultSet resultSet = null;
+
+        preparedStatement = connection.prepareStatement(sql);
+        resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()){
+            Experience experience = new Experience();
+            experience.setUserid(resultSet.getInt("userid"));
+            experience.setTitle(resultSet.getString("title"));
+            experience.setCompanyname(resultSet.getString("companyname"));
+            experience.setLocation(resultSet.getString("location"));
+            experience.setYears(resultSet.getInt("years"));
+            experiences.add(experience);
+        }
+
+        return experiences;
+    }
 }
+
+
+
+
+
+
--- src/main/java/com/example/construct360deg/model/Experience.java
+++ src/main/java/com/example/construct360deg/model/Experience.java
@@ -1,2 +1,49 @@
-package com.example.construct360deg.model;public class Experience {
+package com.example.construct360deg.model;
+
+public class Experience {
+    private int userid;
+    private String title;
+    private String companyname;
+    private String location;
+    private int years;
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompanyname() {
+        return companyname;
+    }
+
+    public void setCompanyname(String companyname) {
+        this.companyname = companyname;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
 }
